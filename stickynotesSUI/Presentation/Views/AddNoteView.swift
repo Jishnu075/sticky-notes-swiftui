27b07cabdfafe2e5518c70c4fa60f576cc3d5bcf
Jishnu Biju <jishnu.biju@litmus7.com>
2025-07-15 02:58:24 +0530

    fab added

--- stickynotesSUI/Presentation/Views/AddNoteView.swift
+++ stickynotesSUI/Presentation/Views/AddNoteView.swift
@@ -12,55 +12,85 @@ struct AddNoteView: View {
     @State private var descriptionText: String = ""
     
     var body: some View {
-        ScrollView {
-            VStack(alignment: .leading) {
-
-                // title
-                ZStack(alignment: .topLeading) {
-                    if titleText.isEmpty {
-                        Text("Title")
+        ZStack {
+            ScrollView {
+                VStack(alignment: .leading) {
+                    // title
+                    ZStack(alignment: .topLeading) {
+                        if titleText.isEmpty {
+                            Text("Title")
+                                .font(.largeTitle)
+                                .foregroundColor(.gray)
+                        }
+                        
+                        TextEditor(text: $titleText)
+                            .opacity(titleText.isEmpty ? 0.25 : 1)
                             .font(.largeTitle)
-                            .foregroundColor(.gray)
+                            .autocorrectionDisabled(true)
+                            .textInputAutocapitalization(.sentences)
+                            .frame(minHeight: 50)
                     }
                     
-                    TextEditor(text: $titleText)
-                        .opacity(titleText.isEmpty ? 0.25 : 1)
-                        .font(.largeTitle)
-                        .autocorrectionDisabled(true)
-                        .textInputAutocapitalization(.sentences)
-                        .frame(minHeight: 50)
-                }
-                
-                // desc
-                ZStack(alignment: .topLeading) {
-                    if descriptionText.isEmpty {
-                        Text("description")
+                    // desc
+                    ZStack(alignment: .topLeading) {
+                        if descriptionText.isEmpty {
+                            Text("description")
+                                .font(.body)
+                                .foregroundColor(.gray)
+                        }
+                        
+                        TextEditor(text: $descriptionText)
+                            .opacity(descriptionText.isEmpty ? 0.25 : 1)
                             .font(.body)
-                            .foregroundColor(.gray)
+                            .autocorrectionDisabled(true)
+                            .textInputAutocapitalization(.sentences)
+                            .frame(minHeight: 50)
                     }
                     
-                    TextEditor(text: $descriptionText)
-                        .opacity(descriptionText.isEmpty ? 0.25 : 1)
-                        .font(.body)
-                        .autocorrectionDisabled(true)
-                        .textInputAutocapitalization(.sentences)
-                        .frame(minHeight: 50)
+                    Spacer()
+                        .frame(height: 80)
                 }
-                
-                Spacer(minLength: 50) // some space at bottom
+                .padding()
             }
-            .padding()
+            
+            // fab
+            if !titleText.isEmpty {
+                VStack {
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        Button(action: {
+                            print("save note")
+                        }) {
+                            //                        Image(systemName: "checkmark")
+                            //                            .font(.title2)
+                            //                            .foregroundColor(.white)
+                            Text("Save")
+                                .font(.footnote)
+                                .foregroundStyle(.white )
+                        }
+                        .frame(width: 56, height: 56)
+                        .background(Color.blue)
+                        .clipShape(Circle())
+                        .shadow(radius: 4)
+                        .padding(.trailing, 16)
+                        .padding(.bottom, 20)
+                        //TODO: add button appearance animations later, a haptic feedback too?
+                    }
+                }
+            }
         }
         .navigationTitle("Add Note")
         .toolbar {
             Button(action: {
                 print("click")
             }, label: {
-                Text("Save")
+                Image(systemName: "pin.fill")
             })
         }
     }
 }
+
 #Preview {
     AddNoteView()
 }
